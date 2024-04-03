0889e3f8d010a6d2afd031937afc6b6059ca9223
Sauravrp67 <98757972+Sauravrp67@users.noreply.github.com>
2024-04-03 18:02:02 +0545

    Added InsertIntoTableGoodData Function

---
+++ Wafer Fault Detection/DataTypeValidaton_Insertion_Training/DataTypeValidation.py
@@ -0,0 +1,153 @@
+import shutil
+import sqlite3
+from datetime import datetime
+from os import listdir
+import os
+import csv
+from application_logging.logger import App_Logger
+
+
+class dBOperation:
+
+    """
+    This class shall be used for handling all the SQL operations.
+
+    Written By: Saurav Raj Paudel
+    Version: 1.0
+    Revisions: None
+
+    """
+    def __init__(self):
+        self.path = 'Training_Database/'
+        self.badFilePath = "Training_Raw_files_validated/Bad_Raw"
+        self.goodFilePath = "Training_Raw_files_validated/Good_Raw"
+        self.logger = App_Logger()
+
+    def dataBaseConnection(self, DatabaseName):
+        """
+            Method Name: dataBaseConnection
+            Description: This method creates the database with the given name and if Database already exists then opens the connection to the DB.
+            Output: Connection to the DB
+            On Failure: Raise ConnectionError
+
+            Written By: Saurav Raj Paudel
+            Version: 1.0
+            Revision: None
+        """
+        try:
+            conn = sqlite3.connect(self.path+DatabaseName+'.db')
+
+            file = open("Training_Logs/DataBaseConnectionLog", 'a+')
+            self.logger.log(file, "Opened %s database sucessfully" % DatabaseName)
+            file.close()
+        except ConnectionError:
+            file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
+            self.logger.log(file, "Error while connecting to database: %s" %ConnectionError)
+            file.close()
+            raise ConnectionError
+        return conn
+    
+    def createTableDb(self, DatabaseName, column_names):
+        """
+                Method Name: CreateTableDb
+                Description: This method creates a table in the given database which will be used to insert the Good data after raw data validation.
+                Output: None
+
+                Written By: Saurav Raj Paudel
+                Version: 1.0
+                Revision: None
+        
+        """
+        try:
+            conn = self.dataBaseConnection(DatabaseName)
+            c = conn.cursor()
+            c.execute("SELECT count(name) FROM sqlite_master WHERE type = 'table'AND name = 'Good_Raw_Data'")
+            if c.fetchone()[0] == 1:
+                conn.close()
+                file = open("Training_Logs/DbTableCreateLog.txt", 'a+')
+                self.logger.log(file, "Tables created sucessfully!!")
+                file.close()
+
+                file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
+                self.logger.log(file, "Close %s database sucessfully" % DatabaseName)
+                file.close()
+
+            else:
+                for key in column_names.key():
+                    type = column_names[key]
+
+                    try:
+                        conn.execute('ALTER TABLE Good_Raw_Data ADD COLUMN "{column_name}" {dataType}'.format(column_name=key,dataType=type))
+                    except:
+                        conn.execute('CREATE TABLE Good_Raw_Data ({column_name} {dataType})'.format(column_name=key, dataType=type))
+                
+                conn.close()
+
+                file = open("Training_Logs/DbTableCreateLog.txt", 'a+')
+                self.logger.log(file, "Table created sucessfully!!")
+                file.close()
+
+                file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
+                self.logger.log(file, "Close %s database successfully" % DatabaseName)
+                file.close()
+        except Exception as e:
+            file = open("Training_Logs/DbTableCreateLog.txt", 'a+')
+            self.logger.log(file, "Error while creating table: %s" % e)
+            file.close()
+            conn.close()
+            file = open("Training_Logs/DataBaseConnectionLog.txt", 'a+')
+            self.logger.log(file, "Close %s database sucessfully" % DatabaseName)
+            file.close()
+            raise e
+        
+    
+
+    def insertIntoTableGoodData(self, Database):
+        """
+            Method Name: insertIntoTableGoodData
+            Description: This method inserts the Good data files from the Good_Raw folder into the 
+                     above created table.
+
+            Output: None
+            On Failure: Raise Exception
+
+            Written By: Saurav Raj Paudel
+            Version: 1.0
+            Revision: None
+    
+        """
+
+        conn = self.dataBaseConnection(Database)
+        goodFilePath = self.goodFilePath
+        badFilePath = self.badFilePath
+        onlyfiles = [f for f in listdir(goodFilePath)]
+        log_file = open("Training_Logs/DbInsertLog.txt", 'a+')
+
+        for file in onlyfiles:
+            try:
+                with open(goodFilePath+'/'+file, "r") as f:
+                    next(f)
+                    reader = csv.reader(f,delimiter= "\n")
+                    for line in enumerate(reader):
+                        for list_ in line[1]:
+                            try:
+                                conn.execute('INSERT INTO Good_Raw_Data values ({values})'.format(values = (list_)))
+                                self.logger.log(log_file, " %s : File loaded successfully!!" %file)
+                                conn.commit()
+                            except Exception as e:
+                                raise e
+            except Exception as e:
+                conn.rollback()
+
+                self.logger.log(log_file, "Error while creating table: %s " % e)
+                shutil.move(goodFilePath+ '/' + file, badFilePath)
+                self.logger.log(log_file, "FIle Moved Successfully %s" % file)
+                log_file.close()
+                conn.close()
+
+
+        conn.close()
+        log_file.close()
+
+
+
--- Wafer Fault Detection/training_Validation_Insertion.py
+++ Wafer Fault Detection/training_Validation_Insertion.py
@@ -18,5 +18,26 @@ class train_validation:
             self.log_writer.log(self.file_object, "Start of Validation on files!!")
             #extracting values from predictin shcema
             LengthOfDateStampInFile, LengthOfTimeStampInFile, column_names, noofcolumns = self.raw_data.valuesFromSchema()
+            #getting the regex defined to validate filename
+            regex = self.raw_data.manualRegexCreation()
+            # validating filename of prediction files
+            self.raw_data.validationFileNameRaw(regex=regex,LengthOfDataStampInFile=LengthOfDateStampInFile,LengthOfTimeStampInFile=LengthOfTimeStampInFile)
+            # validating column length in the file
+            self.raw_data.validateColumnLength(noofcolumns)
+            # validate if any column has all values missing
+            self.raw_data.validateMissingValuesInWholeColumn()
+            self.log_writer.log(self.file_object, "Raw Data validation Complete!!")
+
+            self.log_writer.log(self.file_object, "Starting Data Transformation!!")
+
+            #replacing blanks in the csv file with "Null" values to insert in table
+            self.dataTransform.replaceMissingWithNull()
+
+            self.log_writer.log(self.file_object, "DataTransformation Completed!!")
+
+            self.log_writer.log(self.file_object, "Creating Training_Database and tables on the basis of given schema!!!")
+            # create database with given name, if present open the connection! Create table with columns given in schema
+
+            
         
     
