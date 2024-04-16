eb4823618637150f84aa1fefb3c2be8918538e4d
Sauravrp67 <98757972+Sauravrp67@users.noreply.github.com>
2024-04-16 14:12:17 +0545

    Added Prediction Batch validation

---
+++ Wafer Fault Detection/Prediction_Raw_Data_Validation/predictionDataValidation.py
@@ -0,0 +1,78 @@
+import sqlite3
+from datetime import datetime
+from os import listdir
+import os
+import re
+import json
+import shutil
+import pandas as pd
+from application_logging.logger import App_Logger
+
+
+class Prediction_Data_validation:
+    """
+               This class shall be used for handling all the validation done on the Raw Prediction Data!!.
+
+               Written By: Saurav Raj Paudel
+               Version: 1.0
+               Revisions: None
+
+               """
+
+    def __init__(self,path):
+        self.Batch_Directory = path
+        self.schema_path = 'schema_prediction.json'
+        self.logger = App_Logger()
+
+    def valuesFromSchema(self):
+        """
+                                Method Name: valuesFromSchema
+                                Description: This method extracts all the relevant information from the pre-defined "Schema" file.
+                                Output: LengthOfDateStampInFile, LengthOfTimeStampInFile, column_names, Number of Columns
+                                On Failure: Raise ValueError,KeyError,Exception
+
+                                 Written By: Saurav Raj Paudel
+                                Version: 1.0
+                                Revisions: None
+
+                                        """
+        try:
+            with open(self.schema_path, 'r') as f:
+                dic = json.load(f)
+                f.close()
+            pattern = dic['SampleFileName']
+            LengthOfDateStampInFile = dic['LengthOfDateStampInFile']
+            LengthOfTimeStampInFile = dic['LengthOfTimeStampInFile']
+            column_names = dic['ColName']
+            NumberofColumns = dic['NumberofColumns']
+
+            file = open("Training_Logs/valuesfromSchemaValidationLog.txt", 'a+')
+            message ="LengthOfDateStampInFile:: %s" %LengthOfDateStampInFile + "\t" + "LengthOfTimeStampInFile:: %s" % LengthOfTimeStampInFile +"\t " + "NumberofColumns:: %s" % NumberofColumns + "\n"
+            self.logger.log(file,message)
+
+            file.close()
+
+
+
+        except ValueError:
+            file = open("Prediction_Logs/valuesfromSchemaValidationLog.txt", 'a+')
+            self.logger.log(file,"ValueError:Value not found inside schema_training.json")
+            file.close()
+            raise ValueError
+
+        except KeyError:
+            file = open("Prediction_Logs/valuesfromSchemaValidationLog.txt", 'a+')
+            self.logger.log(file, "KeyError:Key value error incorrect key passed")
+            file.close()
+            raise KeyError
+
+        except Exception as e:
+            file = open("Prediction_Logs/valuesfromSchemaValidationLog.txt", 'a+')
+            self.logger.log(file, str(e))
+            file.close()
+            raise e
+
+        return LengthOfDateStampInFile, LengthOfTimeStampInFile, column_names, NumberofColumns
+
+
+    
