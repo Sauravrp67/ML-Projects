ace4ce5bc2a5c68812a5c8633df7359e69c5af39
Sauravrp67 <98757972+Sauravrp67@users.noreply.github.com>
2024-04-18 13:34:47 +0545

    xgboost best params

--- Wafer Fault Detection/best_model_finder/tuner.py
+++ Wafer Fault Detection/best_model_finder/tuner.py
@@ -65,3 +65,54 @@ class Model_Finder:
             self.logger_object.log(self.file_object,
                                    'Random Forest Parameter tuning  failed. Exited the get_best_params_for_random_forest method of the Model_Finder class')
             raise Exception()
+        
+    def get_best_params_for_xgboost(self,train_x,train_y):
+
+        """
+                                        Method Name: get_best_params_for_xgboost
+                                        Description: get the parameters for XGBoost Algorithm which give the best accuracy.
+                                                     Use Hyper Parameter Tuning.
+                                        Output: The model with the best parameters
+                                        On Failure: Raise Exception
+
+                                        Written By: iNeuron Intelligence
+                                        Version: 1.0
+                                        Revisions: None
+
+                                """
+        self.logger_object.log(self.file_object,
+                               'Entered the get_best_params_for_xgboost method of the Model_Finder class')
+        try:
+            # initializing with different combination of parameters
+            self.param_grid_xgboost = {
+
+                'learning_rate': [0.5, 0.1, 0.01, 0.001],
+                'max_depth': [3, 5, 10, 20],
+                'n_estimators': [10, 50, 100, 200]
+
+            }
+            # Creating an object of the Grid Search class
+            self.grid= GridSearchCV(XGBClassifier(objective='binary:logistic'),self.param_grid_xgboost, verbose=3,cv=5)
+            # finding the best parameters
+            self.grid.fit(train_x, train_y)
+
+            # extracting the best parameters
+            self.learning_rate = self.grid.best_params_['learning_rate']
+            self.max_depth = self.grid.best_params_['max_depth']
+            self.n_estimators = self.grid.best_params_['n_estimators']
+
+            # creating a new model with the best parameters
+            self.xgb = XGBClassifier(learning_rate=self.learning_rate, max_depth=self.max_depth, n_estimators=self.n_estimators)
+            # training the mew model
+            self.xgb.fit(train_x, train_y)
+            self.logger_object.log(self.file_object,
+                                   'XGBoost best params: ' + str(
+                                       self.grid.best_params_) + '. Exited the get_best_params_for_xgboost method of the Model_Finder class')
+            return self.xgb
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   'Exception occured in get_best_params_for_xgboost method of the Model_Finder class. Exception message:  ' + str(
+                                       e))
+            self.logger_object.log(self.file_object,
+                                   'XGBoost Parameter tuning  failed. Exited the get_best_params_for_xgboost method of the Model_Finder class')
+            raise Exception()
