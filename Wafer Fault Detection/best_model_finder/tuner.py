65750340ec75875d9d922ae09c5773be9e383380
Sauravrp67 <98757972+Sauravrp67@users.noreply.github.com>
2024-04-18 13:28:50 +0545

    method for best params for random forest

--- Wafer Fault Detection/best_model_finder/tuner.py
+++ Wafer Fault Detection/best_model_finder/tuner.py
@@ -17,3 +17,51 @@ class Model_Finder:
         self.logger_object = logger_object
         self.clf = RandomForestClassifier()
         self.xgb = XGBClassifier(objective='binary:logistic')
+
+    
+    def get_best_params_for_random_forest(self,train_x,train_y):
+        """
+                                Method Name: get_best_params_for_random_forest
+                                Description: get the parameters for Random Forest Algorithm which give the best accuracy.
+                                             Use Hyper Parameter Tuning.
+                                Output: The model with the best parameters
+                                On Failure: Raise Exception
+
+                                Written By: Saurav Raj Paudel
+                                Version: 1.0
+                                Revisions: None
+
+                        """
+        self.logger_object.log(self.file_object, 'Entered the get_best_params_for_random_forest method of the Model_Finder class')
+        try:
+            # initializing with different combination of parameters
+            self.param_grid = {"n_estimators": [10, 50, 100, 130], "criterion": ['gini', 'entropy'],
+                               "max_depth": range(2, 4, 1), "max_features": ['auto', 'log2']}
+
+            #Creating an object of the Grid Search class
+            self.grid = GridSearchCV(estimator=self.clf, param_grid=self.param_grid, cv=5,  verbose=3)
+            #finding the best parameters
+            self.grid.fit(train_x, train_y)
+
+            #extracting the best parameters
+            self.criterion = self.grid.best_params_['criterion']
+            self.max_depth = self.grid.best_params_['max_depth']
+            self.max_features = self.grid.best_params_['max_features']
+            self.n_estimators = self.grid.best_params_['n_estimators']
+
+            #creating a new model with the best parameters
+            self.clf = RandomForestClassifier(n_estimators=self.n_estimators, criterion=self.criterion,
+                                              max_depth=self.max_depth, max_features=self.max_features)
+            # training the mew model
+            self.clf.fit(train_x, train_y)
+            self.logger_object.log(self.file_object,
+                                   'Random Forest best params: '+str(self.grid.best_params_)+'. Exited the get_best_params_for_random_forest method of the Model_Finder class')
+
+            return self.clf
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   'Exception occured in get_best_params_for_random_forest method of the Model_Finder class. Exception message:  ' + str(
+                                       e))
+            self.logger_object.log(self.file_object,
+                                   'Random Forest Parameter tuning  failed. Exited the get_best_params_for_random_forest method of the Model_Finder class')
+            raise Exception()
